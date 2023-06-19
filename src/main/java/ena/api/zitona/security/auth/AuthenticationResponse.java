bc6297de9ac3143b99dcd0f69e7b95a9097ce744
MohammedSadok <mohammed.sadok.2003@gmail.com>
2023-06-19 10:06:22 +0100

    add jwt implementation

--- src/main/java/ena/api/zitona/security/auth/AuthenticationResponse.java
+++ src/main/java/ena/api/zitona/security/auth/AuthenticationResponse.java
@@ -1,6 +1,7 @@
 package ena.api.zitona.security.auth;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import ena.api.zitona.entitys.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthenticationResponse {
-
     @JsonProperty("access_token")
     private String accessToken;
+    User user;
 }
